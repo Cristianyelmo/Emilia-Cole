33861fe021ab8c1f0fcf137543d56be2dc528948
Cristianyelmo <cristianama2019@gmail.com>
2022-12-06 10:03:53 -0300

    Maquetacion

---
+++ app.js
@@ -0,0 +1,17 @@
+const express =  require('express');
+const app = express();
+const port =3030;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res) =>res.sendFile(path.join(__dirname,'views','index.html')))
+
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname,'views','about.html')))
+
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')))
+
+app.get('/contact',(req,res)=>res.sendFile(path.join(__dirname, 'views','contacto.html')))
+
+
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
